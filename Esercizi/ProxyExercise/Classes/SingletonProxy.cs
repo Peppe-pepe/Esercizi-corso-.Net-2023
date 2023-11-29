dc77ea473f537ca547382bcd69f2419e26f6bec4
Peppe-pepe <g.pepe0455@gmail.com>
2023-11-29 09:50:34 +0100

    ProxyExercise

---
+++ Esercizi/ProxyExercise/Classes/SingletonProxy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyExercise.Classes
+{
+    public class SingletonProxy
+    {
+        private static SingletonProxy instance;
+        private List<string> serversIP;
+        private SingletonProxy() {
+            serversIP = new List<string>() {
+                RandomIPGenerator(),
+                RandomIPGenerator(),
+                RandomIPGenerator(),
+                RandomIPGenerator()
+            };
+        }    
+        private string RandomIPGenerator()
+        {
+            Random gen=new Random();
+            string address = $"{gen.Next(256)}.{gen.Next(256)}.{gen.Next(256)}.{gen.Next(256)}";
+            return address;
+        }
+
+        public static SingletonProxy Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SingletonProxy();
+                return instance;
+            }
+        }
+        public string ServerRequest() {
+            Random gen = new Random();
+            int randomIndex = gen.Next(serversIP.Count);
+            return serversIP[randomIndex];
+        }
+    }
+}
+
---
+++ Esercizi/ProxyExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using ProxyExercise.Classes;
+using System;
+
+namespace ProxyExercise
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                int NInstance = 3;// numero di istanze al proxy
+                int CCall = 2;// numero di chiamate per istanza
+
+                for (int i = 0; i < NInstance; i++)
+                {
+                    SingletonProxy proxy = SingletonProxy.Instance;
+
+                    for (int j = 0; j < CCall; j++)
+                    {
+                        string IndirizzoIP = proxy.ServerRequest();
+                        Console.WriteLine($"Proxy {i + 1}, Chiamata {j + 1}: {IndirizzoIP}");
+
+                    }
+
+                }
+            }
+    }
+}
